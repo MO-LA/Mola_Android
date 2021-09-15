28b96e7e345cfe7a42e6338a7df49417a935786b
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-09-15 17:06:39 +0900

    ADD: main xml

---
+++ app/src/main/java/com/example/molaschoolproject/SchoolDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.molaschoolproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SchoolDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_school_detail)
+    }
+}
