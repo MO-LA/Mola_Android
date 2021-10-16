07e4c62020e6468423b5443805e392a0b6dc6f43
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-17 01:08:24 +0900

    [Add] WordSearchActivity UI
    WordSearchActivity UI

--- app/src/main/java/com/example/molaschoolproject/WordSearchActivity.kt
+++ app/src/main/java/com/example/molaschoolproject/WordSearchActivity.kt
@@ -2,10 +2,16 @@ package com.example.molaschoolproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class WordSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_search)
+
+        val ivWordsearchBack: ImageView = findViewById(R.id.iv_wordsearch_back)
+        ivWordsearchBack.setOnClickListener { finish() }
+
+        
     }
 }
