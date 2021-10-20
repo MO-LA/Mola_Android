6f301904608165d1913096a267fad4012b4c8f3a
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-10-20 20:45:32 +0900

    UPDATE: MyPage

--- app/src/main/java/com/example/molaschoolproject/activity/MyPageActivity.kt
+++ app/src/main/java/com/example/molaschoolproject/activity/MyPageActivity.kt
@@ -1,12 +1,21 @@
 package com.example.molaschoolproject.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import com.example.molaschoolproject.R
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+        val mypick:LinearLayout = findViewById(R.id.layout_mypick)
+
+        mypick.setOnClickListener {
+            val intent = Intent(this@MyPageActivity, MyPickActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
