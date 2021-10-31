10b5f0b1a93fc073a736fb5b3108e1b16550e31f
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-10-31 15:43:31 +0900

    [Add] SchoolAssessmentActivity
    SchoolAssessmentActivity

--- app/src/main/java/com/example/molaschoolproject/activity/SchoolAssessmentActivity.kt
+++ app/src/main/java/com/example/molaschoolproject/activity/SchoolAssessmentActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.molaschoolproject.R
 
-class SchoolRatingActivity : AppCompatActivity() {
+class SchoolAssessmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_school_rating)
+        setContentView(R.layout.activity_school_assessment)
     }
 }
