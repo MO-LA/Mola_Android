b99c2741f7a0cb1e719eb0dba6cede1b12dc12d4
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-11-01 11:37:47 +0900

    [Update] SchoolAssessmentBottomSheet
    SchoolAssessmentBottomSheet

--- app/src/main/java/com/example/molaschoolproject/SchoolAssessmentBottomSheet.kt
+++ app/src/main/java/com/example/molaschoolproject/SchoolAssessmentBottomSheet.kt
@@ -1,4 +1,30 @@
 package com.example.molaschoolproject
 
-class SchoolAssessmentBottomSheet {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class SchoolAssessmentBottomSheet() : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.schoolassessment_dialog,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val starOne = view?.findViewById<ImageView>(R.id.assessment_star_one)
+
+        starOne?.setOnClickListener {
+            starOne.setImageResource(R.drawable.ic_baseline_star_24)
+        }
+    }
 }
