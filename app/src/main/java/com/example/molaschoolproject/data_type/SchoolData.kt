ee3012f4e8420fbedfd471eacbe1aec2fb538c2c
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-11-02 09:36:26 +0900

    [Update] SchoolData
    SchoolData

--- app/src/main/java/com/example/molaschoolproject/data_type/SchoolData.kt
+++ app/src/main/java/com/example/molaschoolproject/data_type/SchoolData.kt
@@ -1,4 +1,9 @@
 package com.example.molaschoolproject.data_type
 
-class SchoolData {
-}
+import java.io.Serializable
+
+data class SchoolData(
+    var data: List<SchoolProfiles>? = null,
+    var message: String? = null,
+    var status: Int? = 0
+): Serializable
