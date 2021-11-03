788ece1163b67ae0263a18a95fada3c87b97cb66
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-03 23:06:02 +0900

    FIX: LocalSearchActivity

--- app/src/main/java/com/example/molaschoolproject/activity/LocalSearchActivity.kt
+++ app/src/main/java/com/example/molaschoolproject/activity/LocalSearchActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
-import com.example.molaschoolproject.MasterApplication
 import com.example.molaschoolproject.R
 import com.example.molaschoolproject.RetrofitService
 import com.example.molaschoolproject.data_type.SignUp
@@ -20,7 +19,6 @@ class LocalSearchActivity : AppCompatActivity() {
 
     var isExistBlank = false
     lateinit var btnNext: Button
-    lateinit var textSkip: TextView
     lateinit var localName: EditText
     lateinit var imgBack: ImageView
 
@@ -31,7 +29,7 @@ class LocalSearchActivity : AppCompatActivity() {
         init(this@LocalSearchActivity)
 
         btnNext.setOnClickListener {
-            signUpLocal(this@LocalSearchActivity)
+            signUpLocal()
         }
 
         imgBack.setOnClickListener {
@@ -40,40 +38,52 @@ class LocalSearchActivity : AppCompatActivity() {
         }
     }
 
-    fun signUpLocal(activity: Activity) {
-        val id = intent.getStringExtra("id")
-        val pw = intent.getStringExtra("pw")
-        val age = intent.getIntExtra("age", 0)
-        val sex = intent.getStringExtra("sex")
-        val schoolName = intent.getStringExtra("schoolName")
-        val localName = localName.text.toString()
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("http://10.80.162.195:8040/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        val service = retrofit.create(RetrofitService::class.java)
-        service.signup(SignUp(id, pw, age, sex, schoolName, localName))
-            .enqueue(object : Callback<SignUp> {
-                override fun onResponse(call: Call<SignUp>, response: Response<SignUp>) {
-                    if (response.isSuccessful) {
-                        //회원가입 -> 학교등록에서 받아오던 정보 getExtra로 받아고 보내기
-                        Toast.makeText(this@LocalSearchActivity, "지역 등록에 성공하셨습니다.", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this@LocalSearchActivity, MainActivity::class.java)
-                        startActivity(intent)
-                    }
-                }
+    fun signUpLocal() {
+        if (localName.text.isEmpty()) {
+            Toast.makeText(this@LocalSearchActivity, "빈칸을 채워주세요.", Toast.LENGTH_SHORT).show()
+        } else {
+            val id = intent.getStringExtra("id")
+            val pw = intent.getStringExtra("pw")
+            val age = intent.getIntExtra("age", 0)
+            val sex = intent.getStringExtra("sex")
+            val schoolName = intent.getStringExtra("schoolName")
+            val localName = localName.text.toString()
 
-                override fun onFailure(call: Call<SignUp>, t: Throwable) {
-                    Toast.makeText(this@LocalSearchActivity, "지역 등록에 실패하셨습니다.", Toast.LENGTH_SHORT).show()
-                }
-            })
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl("http://10.80.162.195:8040/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            val service = retrofit.create(RetrofitService::class.java)
+            service.signup(SignUp(id, pw, age, sex, schoolName, localName))
+                .enqueue(object : Callback<SignUp> {
+                    override fun onResponse(call: Call<SignUp>, response: Response<SignUp>) {
+                        if (response.isSuccessful) {
+                            //회원가입 -> 학교등록에서 받아오던 정보 getExtra로 받아고 보내기
+                            Toast.makeText(
+                                this@LocalSearchActivity,
+                                "지역 등록에 성공하셨습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            val intent = Intent(this@LocalSearchActivity, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<SignUp>, t: Throwable) {
+                        Toast.makeText(
+                            this@LocalSearchActivity,
+                            "지역 등록에 실패하셨습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                })
+        }
     }
 
 
     fun init(activity: Activity) {
         localName = activity.findViewById(R.id.edit_local_name)
         btnNext = activity.findViewById(R.id.btn_next)
-        textSkip = activity.findViewById(R.id.text_skip)
         imgBack = activity.findViewById(R.id.img_back)
     }
 }
