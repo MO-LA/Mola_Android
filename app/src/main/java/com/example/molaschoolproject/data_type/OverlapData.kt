ac28aec34ca71796ffabcbe8c16ec8dc1c0414cd
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-03 23:11:08 +0900

    FIX: OverlapData

---
+++ app/src/main/java/com/example/molaschoolproject/data_type/OverlapData.kt
@@ -0,0 +1,9 @@
+package com.example.molaschoolproject.data_type
+
+import java.io.Serializable
+
+data class OverlapData(
+    var data: Boolean,
+    var massage: String,
+    var status: Int
+) : Serializable
