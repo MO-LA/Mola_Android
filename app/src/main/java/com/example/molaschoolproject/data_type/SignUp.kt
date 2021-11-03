4c16b887d1481e188900ab27d417664e06b7381e
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-03 18:08:56 +0900

    FIX: DataType/SignUp

--- app/src/main/java/com/example/molaschoolproject/data_type/SignUp.kt
+++ app/src/main/java/com/example/molaschoolproject/data_type/SignUp.kt
@@ -5,7 +5,7 @@ import java.io.Serializable
 class SignUp(
     var id: String? = null,
     var password: String? = null,
-    var age: Int? = null,
+    var age: Int? = 0,
     var sex: String? = null,
     var residentialArea: String? = null,
     var school: String? = null
