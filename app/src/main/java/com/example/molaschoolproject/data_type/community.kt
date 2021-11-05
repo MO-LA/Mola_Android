476c309159ef18e013b68e228a7da10a469d5111
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-05 10:57:41 +0900

    ADD: Community

--- app/src/main/java/com/example/molaschoolproject/data_type/community.kt
+++ app/src/main/java/com/example/molaschoolproject/data_type/community.kt
@@ -2,10 +2,11 @@ package com.example.molaschoolproject.data_type
 
 import java.io.Serializable
 
-data class community (
+class Community(
     val content: String? = null,
     val dateTime: String? = null,
     val id: String? = null,
     val idx: Int? = 0,
-    val school: String? = null
+    val school: String? = null,
+    val title: String? = null
 ) : Serializable
