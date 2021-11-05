359e1a895bc0f4dd6902f5a4afa12a5e2ad54a3d
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-05 23:37:39 +0900

    UPDATE: DataType/Community

--- app/src/main/java/com/example/molaschoolproject/data_type/community.kt
+++ app/src/main/java/com/example/molaschoolproject/data_type/community.kt
@@ -1,6 +1,8 @@
 package com.example.molaschoolproject.data_type
 
 import java.io.Serializable
+import java.time.LocalDateTime
+import java.util.*
 
 class Community(
     val content: String? = null,
