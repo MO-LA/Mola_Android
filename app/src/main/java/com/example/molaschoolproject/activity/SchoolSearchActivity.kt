8d10546205f5b76bce37384e7baebb7903a1b31a
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-07 02:57:41 +0900

    Update SchoolSearchActivity.kt

--- app/src/main/java/com/example/molaschoolproject/activity/SchoolSearchActivity.kt
+++ app/src/main/java/com/example/molaschoolproject/activity/SchoolSearchActivity.kt
@@ -1,6 +1,7 @@
 package com.example.molaschoolproject.activity
 
 import android.app.Activity
+import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,8 +10,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.molaschoolproject.R
 import com.example.molaschoolproject.RetrofitService
+import com.example.molaschoolproject.adapter.SchoolSearchAdapter
+import com.example.molaschoolproject.data_type.SchoolData
+import com.example.molaschoolproject.data_type.SchoolProfiles
 import com.example.molaschoolproject.data_type.SignUp
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,15 +33,63 @@ class SchoolSearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_school_search)
 
+        val rvSchoolSearch = findViewById<RecyclerView>(R.id.rv_school)
+        rvSchoolSearch.layoutManager =
+            LinearLayoutManager(this@SchoolSearchActivity, LinearLayoutManager.VERTICAL, false)
+        rvSchoolSearch.setHasFixedSize(true)
+
         init()
 
         btnNext.setOnClickListener {
             Toast.makeText(this@SchoolSearchActivity, "학교 설정이 완료되었습니다.", Toast.LENGTH_SHORT).show()
             signUpSchool()
         }
+
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.61.124:8040/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val schoolSearchService = retrofit.create(RetrofitService::class.java)
+
+        schoolSearchService.getSchoolData().enqueue(object : Callback<SchoolData> {
+            override fun onResponse(call: Call<SchoolData>, response: Response<SchoolData>) {
+                Log.d("Retrofitt","SchoolSearch code = ${response.code()}")
+                if(response.isSuccessful){
+                    val schoolSearchList = response.body()?.data
+                    rvSchoolSearch.adapter = SchoolSearchAdapter(schoolSearchList as ArrayList<SchoolProfiles>)
+                }
+            }
+
+            override fun onFailure(call: Call<SchoolData>, t: Throwable) {
+
+            }
+        })
+
+        schoolName.setOnClickListener {
+            val searchService = retrofit.create(RetrofitService::class.java)
+            var searchSchoolData: String = schoolName.text.toString()
+            if (searchSchoolData.isEmpty())  searchSchoolData = ""
+            searchSchoolData = searchSchoolData.replace(" ","")
+            searchService.getSchoolDataByName(q = searchSchoolData).enqueue(object : Callback<SchoolData> {
+
+                override fun onResponse(call: Call<SchoolData>, response: Response<SchoolData>) {
+                    Log.d("Retrofitt","searchByName main code = ${response.code()}")
+                    if(response.isSuccessful){
+                        val schoolSearchList = response.body()?.data
+                        rvSchoolSearch.adapter =  SchoolSearchAdapter(schoolSearchList as ArrayList<SchoolProfiles>)
+                    }
+                }
+
+                override fun onFailure(call: Call<SchoolData>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
     }
 
-    private fun signUpSchool() {
+    fun signUpSchool() {
         if (schoolName.text.isEmpty()) {
             Toast.makeText(this@SchoolSearchActivity, "빈칸을 채워주세요.", Toast.LENGTH_SHORT).show()
         } else {
@@ -53,7 +107,6 @@ class SchoolSearchActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
 
     fun init() {
         schoolName = findViewById(R.id.edit_school_name)
