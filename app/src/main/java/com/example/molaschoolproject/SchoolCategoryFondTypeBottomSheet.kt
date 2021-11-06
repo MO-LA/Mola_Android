4ba3135bc8448b507e5bcc2135c9734794c8814b
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-11-06 11:00:36 +0900

    [Update] SchoolCategoryFondTypeBottomSheet
    SchoolCategoryFondTypeBottomSheet

--- app/src/main/java/com/example/molaschoolproject/SchoolCategoryFondTypeBottomSheet.kt
+++ app/src/main/java/com/example/molaschoolproject/SchoolCategoryFondTypeBottomSheet.kt
@@ -1,4 +1,37 @@
 package com.example.molaschoolproject
 
-class SchoolCategoryFondTypeBottomSheet {
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class SchoolCategoryFondTypeBottomSheet() : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.schoolcategory_fondtype_dialog, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+    interface textClickListener {
+        fun onClicked(fondText: String)
+    }
+
+    private lateinit var onClickedListener: textClickListener
+
+    fun setOnClickedListener(listener:textClickListener){
+        onClickedListener = listener
+    }
 }
