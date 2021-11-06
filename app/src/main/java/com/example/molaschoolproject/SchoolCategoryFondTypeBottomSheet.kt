618bf788060f97958292a84df97edd6ee57e9a9f
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-11-06 11:15:05 +0900

    [Update] SchoolCategoryFondTypeBottomSheet
    SchoolCategoryFondTypeBottomSheet

--- app/src/main/java/com/example/molaschoolproject/SchoolCategoryFondTypeBottomSheet.kt
+++ app/src/main/java/com/example/molaschoolproject/SchoolCategoryFondTypeBottomSheet.kt
@@ -22,11 +22,58 @@ class SchoolCategoryFondTypeBottomSheet() : BottomSheetDialogFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        var fondTypeText: String = "설립유형"
+
+        val cateFondTypeAll: TextView? = view?.findViewById(R.id.cate_fondtype_all)
+        val cateIndependence: TextView? = view?.findViewById(R.id.cate_independence)
+        val cateEstablish: TextView? = view?.findViewById(R.id.cate_establish)
+        val cateAccessories: TextView? = view?.findViewById(R.id.cate_accessories)
+
+        view?.findViewById<Button>(R.id.button_category_fondtype_bottom_sheet)?.setOnClickListener{
+            onClickedListener.onClicked(fondTypeText)
+            dismiss()
+        }
+
+        cateFondTypeAll?.setOnClickListener{
+            cateFondTypeAll.setTextColor(Color.BLUE)
+            cateIndependence?.setTextColor(Color.BLACK)
+            cateEstablish?.setTextColor(Color.BLACK)
+            cateAccessories?.setTextColor(Color.BLACK)
+
+            fondTypeText = cateFondTypeAll.text.toString()
+        }
+
+        cateIndependence?.setOnClickListener{
+            cateFondTypeAll?.setTextColor(Color.BLACK)
+            cateIndependence.setTextColor(Color.BLUE)
+            cateEstablish?.setTextColor(Color.BLACK)
+            cateAccessories?.setTextColor(Color.BLACK)
+
+            fondTypeText = cateIndependence.text.toString()
+        }
+
+        cateEstablish?.setOnClickListener{
+            cateFondTypeAll?.setTextColor(Color.BLACK)
+            cateIndependence?.setTextColor(Color.BLACK)
+            cateEstablish.setTextColor(Color.BLUE)
+            cateAccessories?.setTextColor(Color.BLACK)
+
+            fondTypeText = cateEstablish.text.toString()
+        }
+
+        cateAccessories?.setOnClickListener{
+            cateFondTypeAll?.setTextColor(Color.BLACK)
+            cateIndependence?.setTextColor(Color.BLACK)
+            cateEstablish?.setTextColor(Color.BLACK)
+            cateAccessories.setTextColor(Color.BLUE)
+
+            fondTypeText = cateAccessories.text.toString()
+        }
 
     }
 
     interface textClickListener {
-        fun onClicked(fondText: String)
+        fun onClicked(fondTypeText: String)
     }
 
     private lateinit var onClickedListener: textClickListener
