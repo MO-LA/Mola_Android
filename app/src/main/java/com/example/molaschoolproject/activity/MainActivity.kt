0bcccef9314c94f97b5f439fd3bdda0b2b9ac589
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-11-07 21:54:28 +0900

    [Update] Retrofit baseURL
    Retrofit baseURL

--- app/src/main/java/com/example/molaschoolproject/activity/MainActivity.kt
+++ app/src/main/java/com/example/molaschoolproject/activity/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         val schoolCategoryRegion: TextView = findViewById(R.id.tv_region)
 
         val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("http://192.168.61.124:8040/")
+            .baseUrl("http://10.80.162.195:8040/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
--- app/src/main/java/com/example/molaschoolproject/activity/SchoolDetailActivity.kt
+++ app/src/main/java/com/example/molaschoolproject/activity/SchoolDetailActivity.kt
@@ -44,7 +44,7 @@ class SchoolDetailActivity : AppCompatActivity() {
         val okHttpClient = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
         val retrofit: Retrofit = Retrofit.Builder()
             .client(okHttpClient)
-            .baseUrl("http://192.168.61.124:8040/")
+            .baseUrl("http://10.80.162.195:8040/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
