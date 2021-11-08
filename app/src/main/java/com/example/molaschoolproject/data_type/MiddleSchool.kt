de575f0aea425fa5b9bef4949c982d3d2f4fd6b0
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-08 22:51:11 +0900

    Create MiddleSchool.kt

---
+++ app/src/main/java/com/example/molaschoolproject/data_type/MiddleSchool.kt
@@ -0,0 +1,8 @@
+package com.example.molaschoolproject.data_type
+
+import java.io.Serializable
+
+class MiddleSchool(
+    val name: String? = null,
+    val roadAddressName: String? = null
+): Serializable
