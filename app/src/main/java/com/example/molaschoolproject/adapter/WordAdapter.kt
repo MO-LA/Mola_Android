f31c1b86df9c5641eb48e3f09ada05f7d0eef51d
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-09 08:45:03 +0900

    Update WordAdapter.kt

--- app/src/main/java/com/example/molaschoolproject/adapter/WordAdapter.kt
+++ app/src/main/java/com/example/molaschoolproject/adapter/WordAdapter.kt
@@ -1,5 +1,10 @@
 package com.example.molaschoolproject.adapter
 
+import android.text.Html
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.URLSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +12,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.molaschoolproject.R
 import com.example.molaschoolproject.data_type.Word
+
 
 class WordAdapter(val wordList: ArrayList<Word>) : RecyclerView.Adapter<WordAdapter.CustomViewHolder>() {
 
@@ -19,6 +25,13 @@ class WordAdapter(val wordList: ArrayList<Word>) : RecyclerView.Adapter<WordAdap
         holder.wordName.text = wordList.get(position).wordName
         holder.wordContents.text = wordList.get(position).wordContents
         holder.wordLink.text = wordList.get(position).wordLink
+
+//        val content = SpannableString(holder.wordLink.text.toString())
+//        val url = "https://namu.wiki/w/%EC%9D%BC%EB%B0%98%EA%B3%84%20%EA%B3%A0%EB%93%B1%ED%95%99%EA%B5%90"
+//        content.setSpan(URLSpan(url), 0, content.length, 0)
+//        holder.wordLink.setMovementMethod(LinkMovementMethod.getInstance())
+//        holder.wordLink.text = content
+//        holder.wordLink.text = wordList.get(position).wordLink
     }
 
     override fun getItemCount(): Int {
@@ -28,6 +41,8 @@ class WordAdapter(val wordList: ArrayList<Word>) : RecyclerView.Adapter<WordAdap
         val wordName = itemView.findViewById<TextView>(R.id.tv_wordname) // 용어 이름
         val wordContents = itemView.findViewById<TextView>(R.id.tv_wordcontents) // 용어 설명 내용
         val wordLink = itemView.findViewById<TextView>(R.id.tv_word_weblink)
+
+
     }
 
 
