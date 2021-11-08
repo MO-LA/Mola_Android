2beb1580a86dbad3dbf50da2466f4f8dac3dcc9b
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-08 22:51:19 +0900

    Create MiddleSchoolData.kt

---
+++ app/src/main/java/com/example/molaschoolproject/data_type/MiddleSchoolData.kt
@@ -0,0 +1,10 @@
+package com.example.molaschoolproject.data_type
+
+import java.io.Serializable
+
+data class MiddleSchoolData(
+    val data: List<MiddleSchool>,
+    val massage: String,
+    val status: Int
+): Serializable
+
