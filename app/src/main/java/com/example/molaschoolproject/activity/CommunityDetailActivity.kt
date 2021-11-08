6049a36af5c3e07013fc6fd92eec4e561b9475ba
JJ7224 <63018169+JJ7224@users.noreply.github.com>
2021-11-08 13:26:03 +0900

    Update CommunityDetailActivity.kt

--- app/src/main/java/com/example/molaschoolproject/activity/CommunityDetailActivity.kt
+++ app/src/main/java/com/example/molaschoolproject/activity/CommunityDetailActivity.kt
@@ -1,18 +1,24 @@
 package com.example.molaschoolproject.activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.molaschoolproject.AuthInterceptor
 import com.example.molaschoolproject.R
 import com.example.molaschoolproject.RetrofitService
+import com.example.molaschoolproject.adapter.ReviewAdapter
+import com.example.molaschoolproject.data_type.Comment
+import com.example.molaschoolproject.data_type.CommentWrite
 import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -24,49 +30,105 @@ class CommunityDetailActivity : AppCompatActivity() {
     lateinit var detailContent: TextView
     lateinit var editReview: EditText
     lateinit var imgBtnSend: ImageButton
-    private lateinit var imgBtnBack: ImageButton
-    lateinit var rv_review: RecyclerView
+    lateinit var imgBtnBack: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community_detail)
 
         init()
-        rv_review.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-        rv_review.setHasFixedSize(true)
+
+        val rvReview = findViewById<RecyclerView>(R.id.rv_review)
+        rvReview.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rvReview.setHasFixedSize(true)
 
         setText()
-    }
 
-    fun communityWrite() {
+        val postIdx: Int = intent.getIntExtra("idx", 0)
+        Toast.makeText(this@CommunityDetailActivity, "postIdx = $postIdx", Toast.LENGTH_SHORT).show()
+
+        imgBtnBack.setOnClickListener {
+            val intent = Intent(this, CommunityActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
+
+
         val okHttpClient = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
-
         val retrofit: Retrofit = Retrofit.Builder()
             .client(okHttpClient)
             .baseUrl("http://10.80.162.195:8040/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+        val commentService = retrofit.create(RetrofitService::class.java)
 
-        val service = retrofit.create(RetrofitService::class.java)
+        imgBtnSend.setOnClickListener {
+            if (editReview.text.isEmpty()) {
+                Toast.makeText(this@CommunityDetailActivity, "빈칸을 채워주세요.", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                var reviewContent = editReview.text.toString()
+                commentService.postComment(CommentWrite(postIdx, reviewContent))
+                    .enqueue(object : Callback<Any?> {
+                        override fun onResponse(call: Call<Any?>, response: Response<Any?>) {
+                            Log.d("retrofitt", "comment post retrofit code = ${response.code()}")
+                            if(response.isSuccessful) {
+                                val getCommentService = retrofit.create(RetrofitService::class.java)
+                                getCommentService.getComment(postIdx).enqueue(object : Callback<Comment?> {
+                                    override fun onResponse(call: Call<Comment?>, response: Response<Comment?>) {
+                                        Log.d("Retrofitt", "get comment code = ${response.code()}")
+                                        val commentList = response.body()?.data
+                                        rvReview.adapter = commentList?.let { ReviewAdapter(it) }
+                                    }
+
+                                    override fun onFailure(call: Call<Comment?>, t: Throwable) {
+                                        Log.d("retrofitt", "get comment retrofit false")
+                                    }
+                                })
+                            }
+
+                        }
+                        override fun onFailure(call: Call<Any?>, t: Throwable) {
+                            Log.d("retrofitt", "comment post retrofit false")
+                        }
+                    })
+                editReview.text = null
+            }
+        }
+        val getCommentService = retrofit.create(RetrofitService::class.java)
+        getCommentService.getComment(postIdx).enqueue(object : Callback<Comment?> {
+            override fun onResponse(call: Call<Comment?>, response: Response<Comment?>) {
+                Log.d("Retrofitt", "get comment code = ${response.code()}")
+                val commentList = response.body()?.data
+                rvReview.adapter = commentList?.let { ReviewAdapter(it) }
+            }
+
+            override fun onFailure(call: Call<Comment?>, t: Throwable) {
+                Log.d("retrofitt", "get comment retrofit false")
+            }
+        })
     }
 
     fun setText() {
         detailTitle.text = intent.getStringExtra("title")
         detailTime.text = intent.getStringExtra("dateTime")
-        detailId.text = intent.getStringExtra("postIdx")
+        detailId.text = intent.getStringExtra("id")
         detailContent.text = intent.getStringExtra("content")
+    }
 
-        val postIdx: Int = intent.getIntExtra("idx", 0)
-        Toast.makeText(this,"postIdx = $postIdx", Toast.LENGTH_SHORT).show()
+    override fun onBackPressed() {
+        val intent: Intent = Intent(this, CommunityActivity::class.java)
+        finish()
+        startActivity(intent)
     }
 
     fun init() {
         detailTitle = findViewById(R.id.txt_community_detail_title)
         detailTime = findViewById(R.id.txt_community_detail_time)
-        detailId= findViewById(R.id.txt_community_detail_id)
+        detailId = findViewById(R.id.txt_community_detail_id)
         detailContent = findViewById(R.id.txt_community_detail_content)
         editReview = findViewById(R.id.edit_review)
         imgBtnSend = findViewById(R.id.img_btn_review_send)
-        rv_review = findViewById(R.id.rv_review)
+        imgBtnBack = findViewById(R.id.img_detail_back)
     }
 }
