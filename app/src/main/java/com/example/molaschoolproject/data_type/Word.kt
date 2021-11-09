c760a1b8f886ccb01e06e878cf431e7e91275f14
hwaun123 <67179490+hwaun123@users.noreply.github.com>
2021-11-09 09:15:57 +0900

    [Update] Word Class
    Word Class

--- app/src/main/java/com/example/molaschoolproject/data_type/Word.kt
+++ app/src/main/java/com/example/molaschoolproject/data_type/Word.kt
@@ -1,3 +1,3 @@
 package com.example.molaschoolproject.data_type
 
-class Word (val wordName: String, val wordContents: String, val wordLink: String)
+class Word (val wordName: String, val wordContents: String, val wordLink: String,val urlLink: String)
